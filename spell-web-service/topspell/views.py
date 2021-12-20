9ada4052a6b8f6593683ed53ce5cc8718621ded0
Mehrdad HNA <58226760+MR-EIGHT@users.noreply.github.com>
2021-12-20 16:50:04 +0330

    top spell app made and configured.

---
+++ spell-web-service/topspell/views.py
@@ -0,0 +1,8 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+from django.http import HttpRequest
+# Create your views here.
+
+
+def he(request):
+    return render(request,'index.html')
