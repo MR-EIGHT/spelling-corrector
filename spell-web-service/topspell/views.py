d57e68094fb041d92f07bd3e9b1a787a3762296f
Mehrdad HNA <58226760+MR-EIGHT@users.noreply.github.com>
2021-12-31 04:52:33 +0330

    views file improved & bugs fixed.

--- spell-web-service/topspell/views.py
+++ spell-web-service/topspell/views.py
@@ -1,3 +1,4 @@
+from typing import Text
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http import HttpRequest
@@ -8,7 +9,11 @@ from topspell.Corrector import Corrector
 def he(request):
     data = request.POST.get('text')
     print(data)
-    return render(request,'index.html',{'data': data if data is None else list_of_possible_spellings(text=data)})
+    ans=list_of_possible_spellings(text=data)
+    if len(data.split())>1:
+        return render(request,'index.html',{'data': data if data is None else ans[1],'answer':' '.join(ans[0])})
+    else:
+        return render(request,'index.html',{'data': data if data is None else ans[1],'answer':ans[0]})
 
 def list_of_possible_spellings(text):
     Cor = Corrector()
